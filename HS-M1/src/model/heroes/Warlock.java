44a44d8fbd8eb083b4f07559e120a4e7eddbb872
Hadi-Elnimr <46900958+Hadi-Elnimr@users.noreply.github.com>
2020-03-23 01:14:14 +0200

    Wilfred Fizzlebang Done
    Needs to be revised.
    drawCard() in Hero seems to be incorrect.

--- HS-M1/src/model/heroes/Warlock.java
+++ HS-M1/src/model/heroes/Warlock.java
@@ -9,6 +9,7 @@ import exceptions.FullHandException;
 import exceptions.HeroPowerAlreadyUsedException;
 import exceptions.NotEnoughManaException;
 import exceptions.NotYourTurnException;
+import model.cards.Card;
 import model.cards.Rarity;
 import model.cards.minions.Minion;
 import model.cards.spells.CurseOfWeakness;
@@ -23,25 +24,46 @@ public class Warlock extends Hero {
 
 	@Override
 	public void buildDeck() throws IOException {
-		ArrayList<Minion> neutrals= getNeutralMinions(getAllNeutralMinions("neutral_minions.csv"),13);
+		ArrayList<Minion> neutrals = getNeutralMinions(getAllNeutralMinions("neutral_minions.csv"), 13);
 		getDeck().addAll(neutrals);
-		for(int i = 0 ; i < 2; i++)
-		{
+		for (int i = 0; i < 2; i++) {
 			getDeck().add(new CurseOfWeakness());
 			getDeck().add(new SiphonSoul());
 			getDeck().add(new TwistingNether());
 		}
-		Minion wilfred=new Minion("Wilfred Fizzlebang",6,Rarity.LEGENDARY,4,4,false,false,false);
+		Minion wilfred = new Minion("Wilfred Fizzlebang", 6, Rarity.LEGENDARY, 4, 4, false, false, false);
 		getDeck().add(wilfred);
 		Collections.shuffle(getDeck());
 
 	}
-	
-	public void useHeroPower() throws NotEnoughManaException, HeroPowerAlreadyUsedException, NotYourTurnException, FullHandException, FullFieldException, CloneNotSupportedException
-	 {
-		 super.useHeroPower();
-		 drawCard();
-		 setCurrentHP(getCurrentHP()-2);
-	 }
-	
+
+	public void checkFizzlebangOnField(Minion minion) {
+		for (Minion m : getField())
+			if (m.getName().equals("Wilfred Fizzlebang")) {
+				minion.setManaCost(0);
+				return;
+			}
+	}
+
+	@Override
+	public Card drawCard() throws FullHandException, CloneNotSupportedException {
+		// FizzleBang
+		Card c = super.drawCard();
+		if (c == null) // Empty deck
+			return null;
+		if (c instanceof Minion)
+			checkFizzlebangOnField((Minion) c);
+
+		// Chromaggus to be added
+
+		return c;
+	}
+
+	public void useHeroPower() throws NotEnoughManaException, HeroPowerAlreadyUsedException, NotYourTurnException,
+			FullHandException, FullFieldException, CloneNotSupportedException {
+		super.useHeroPower();
+		Card c = drawCard();
+		setCurrentHP(getCurrentHP() - 2);
+	}
+
 }
