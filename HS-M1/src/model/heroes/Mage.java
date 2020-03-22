67d4489f6488cf6cd0b73799d0331df0540a9005
Hadi-Elnimr <46900958+Hadi-Elnimr@users.noreply.github.com>
2020-03-23 00:55:14 +0200

    Kalycgos
    Needs Revision

--- HS-M1/src/model/heroes/Mage.java
+++ HS-M1/src/model/heroes/Mage.java
@@ -4,16 +4,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import exceptions.FullFieldException;
-import exceptions.FullHandException;
-import exceptions.HeroPowerAlreadyUsedException;
-import exceptions.NotEnoughManaException;
-import exceptions.NotYourTurnException;
+import exceptions.*;
 import model.cards.Rarity;
 import model.cards.minions.Minion;
-import model.cards.spells.Flamestrike;
-import model.cards.spells.Polymorph;
-import model.cards.spells.Pyroblast;
+import model.cards.spells.*;
 
 public class Mage extends Hero {
 
@@ -23,7 +17,7 @@ public class Mage extends Hero {
 
 	@Override
 	public void buildDeck() throws IOException {
-		ArrayList<Minion> neutrals = getNeutralMinions(getAllNeutralMinions("neutral_minions.csv"),13);
+		ArrayList<Minion> neutrals = getNeutralMinions(getAllNeutralMinions("neutral_minions.csv"), 13);
 		getDeck().addAll(neutrals);
 		for (int i = 0; i < 2; i++) {
 			getDeck().add(new Polymorph());
@@ -36,14 +30,48 @@ public class Mage extends Hero {
 		Collections.shuffle(getDeck());
 
 	}
-	public void useHeroPower(Object target) throws NotEnoughManaException, HeroPowerAlreadyUsedException, NotYourTurnException, FullHandException, FullFieldException, CloneNotSupportedException
-	 {
+
+	public void checkKalycgosOnField(Spell s) { // Legendary Minion Special effects
+		for (Minion m : getField())
+			if (m.getName().equals("Kalycgos")) {
+				s.setManaCost(s.getManaCost() - 4);
+				return;
+			}
+	}
+
+	public void castSpell(FieldSpell s) throws NotYourTurnException, NotEnoughManaException {
+		checkKalycgosOnField((Spell) s);
+		super.castSpell(s);
+	}
+
+	public void castSpell(AOESpell s, ArrayList<Minion> oppField) throws NotYourTurnException, NotEnoughManaException {
+		checkKalycgosOnField((Spell) s);
+		super.castSpell(s, oppField);
+	}
+
+	public void castSpell(MinionTargetSpell s, Minion m)
+			throws NotYourTurnException, NotEnoughManaException, InvalidTargetException {
+		checkKalycgosOnField((Spell) s);
+		super.castSpell(s, m);
+	}
+
+	public void castSpell(HeroTargetSpell s, Hero h) throws NotYourTurnException, NotEnoughManaException {
+		checkKalycgosOnField((Spell) s);
+		super.castSpell(s, h);
+	}
+
+	public void castSpell(LeechingSpell s, Minion m) throws NotYourTurnException, NotEnoughManaException {
+		checkKalycgosOnField((Spell) s);
+		super.castSpell(s, m);
+	}
+
+	public void useHeroPower(Object target) throws NotEnoughManaException, HeroPowerAlreadyUsedException,
+			NotYourTurnException, FullHandException, FullFieldException, CloneNotSupportedException {
 		super.useHeroPower();
-		if(target instanceof Minion)
-			((Minion)target).setCurrentHP(((Minion)target).getCurrentHP()-1);
-		else if(target instanceof Hero)
-			((Hero)target).setCurrentHP(((Hero)target).getCurrentHP()-1);
-	 }
-	
+		if (target instanceof Minion)
+			((Minion) target).setCurrentHP(((Minion) target).getCurrentHP() - 1);
+		else if (target instanceof Hero)
+			((Hero) target).setCurrentHP(((Hero) target).getCurrentHP() - 1);
+	}
 
 }
