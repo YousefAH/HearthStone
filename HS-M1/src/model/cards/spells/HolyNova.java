5640d83caf11d2c5b92971fb6c2f5684c9806b15
YousefAH <yousefhmd584@gmail.com>
2020-03-22 17:24:16 +0200

    Holy Nova & Siphon Soul

--- HS-M1/src/model/cards/spells/HolyNova.java
+++ HS-M1/src/model/cards/spells/HolyNova.java
@@ -1,12 +1,37 @@
 package model.cards.spells;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import model.cards.Rarity;
+import model.cards.minions.Minion;
 
 public class HolyNova extends Spell implements AOESpell {
 
 	public HolyNova() {
 		super("Holy Nova", 5, Rarity.BASIC);
 	
+	}
+
+	@Override
+	public void performAction(ArrayList<Minion> oppField, ArrayList<Minion> curField) {
+		for (int i = 0; i < oppField.size(); i++) 
+		{
+			oppField.get(i).setCurrentHP(oppField.get(i).getCurrentHP()-2);
+		}
+		Iterator itr = oppField.iterator();
+		 while (itr.hasNext()) 
+	     { 
+			Minion check = (Minion)itr.next();
+			if(check.getCurrentHP()==0)
+				itr.remove();
+	     } 
+		
+		
+		for (int i = 0; i < curField.size(); i++) 
+		{
+			curField.get(i).setCurrentHP(curField.get(i).getCurrentHP()+2);
+		}
 	}
 
 }
--- HS-M1/src/model/cards/spells/TwistingNether.java
+++ HS-M1/src/model/cards/spells/TwistingNether.java
@@ -1,12 +1,22 @@
 package model.cards.spells;
 
+import java.util.ArrayList;
+
 import model.cards.Rarity;
+import model.cards.minions.Minion;
 
 public class TwistingNether extends Spell implements AOESpell {
 
 	public TwistingNether() {
 		super("Twisting Nether", 8, Rarity.EPIC);
 
+	}
+
+	
+	public void performAction(ArrayList<Minion> oppField, ArrayList<Minion> curField) 
+	{
+		curField.clear();
+		oppField.clear();
 	}
 
 }
