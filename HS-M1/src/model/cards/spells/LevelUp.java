298f8efb895dfc1e73d36e72b6150c8583b24bb2
YousefAH <yousefhmd584@gmail.com>
2020-03-25 05:21:26 +0200

    fixed levelUp

--- HS-M1/src/model/cards/spells/LevelUp.java
+++ HS-M1/src/model/cards/spells/LevelUp.java
@@ -17,8 +17,9 @@ public class LevelUp extends Spell implements FieldSpell {
 		for (Minion m : field)
 			if (m.getName().equals("Silver Hand Recruit")) {
 				m.setAttack(m.getAttack() + 1);
-				m.setCurrentHP(m.getCurrentHP() + 1);
 				m.setMaxHP(m.getMaxHP() + 1);
+				m.setCurrentHP(m.getCurrentHP() + 1);
+				
 			}
 	}
 
